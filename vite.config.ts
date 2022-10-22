33ae2769b4537975a8267d7dd90be514548bb939
2-one-week <2-one-week@naver.com>
2022-10-23 01:39:27 +0900

    vite ts config alias 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+
+const tsconfig = require('./tsconfig.json')
+
+type TSConfig = {
+    compilerOptions: {baseUrl: string; paths: {[key: string]: string[]}}
+}
+
+const {
+    compilerOptions: {baseUrl, paths},
+}: TSConfig = tsconfig
+function getTsConfigAlias() {
+    const alias: {[key in string]: string} = {}
+
+    for (const [aliasPath, [rawRelativePath]] of Object.entries(paths)) {
+        const relativePathWithBaseUrl = `${baseUrl}/${rawRelativePath}`
+        const relativePath = relativePathWithBaseUrl.replace(/\/\*$/, '')
+        const absolutePath = path.resolve(__dirname, relativePath)
+        const aliasKey = aliasPath.replace(/\/\*$/, '')
+        alias[aliasKey] = absolutePath
+    }
+
+    return alias
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    resolve: {
+        alias: getTsConfigAlias(),
+    },
     plugins: [react()],
 })
